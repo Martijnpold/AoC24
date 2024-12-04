77b9167889d11e18a3cbdd452009ec00f6076370
Martijn Polderman <mpolderman@bol.com>
2024-12-04 11:41:22 +0100

    Solve day 3

---
+++ src/main/kotlin/day3.kt
@@ -0,0 +1,40 @@
+import util.loadLines
+
+fun main() {
+    val lines = loadLines("day3.txt")
+
+    partOne(lines)
+    partTwo(lines)
+}
+
+private fun partOne(lines: List<String>) {
+    val regex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+
+    val result = lines.sumOf { line ->
+        regex.findAll(line).sumOf { match ->
+            match.groups[1]!!.value.toInt() * match.groups[2]!!.value.toInt()
+        }
+    }
+
+    println("day 3-1 = $result")
+}
+
+private fun partTwo(lines: List<String>) {
+    val regex = """(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))""".toRegex()
+
+    var enabled = true
+    val result = lines.sumOf { line ->
+        regex.findAll(line).sumOf { match ->
+            when(match.groupValues[1]) {
+                "don't()" -> enabled = false
+                "do()" -> enabled = true
+                else -> {
+                    if(enabled) return@sumOf match.groups[2]!!.value.toInt() * match.groups[3]!!.value.toInt()
+                }
+            }
+            0
+        }
+    }
+
+    println("day 3-2 = $result")
+}
